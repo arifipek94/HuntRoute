c979d4715b936956aafc2144b61823e686262e64
arifipek94 <arifipek94@gmail.com>
2025-09-17 17:07:26 +0200

    added axios to the dependencies

--- globe-fare-adaptive/utils/getAirportName.js
+++ globe-fare-adaptive/utils/getAirportName.js
@@ -1,5 +1,5 @@
 import iataData from "../data/iata-data.json" with { type: "json" };
-import airports from "./airports.json" assert { type: "json" };
+import airports from "./airports.json" with { type: "json" };
 
 function getAirportName(code) {
   const entry = iataData[code];
